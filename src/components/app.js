fcb6738d20757e0fda416c191b69c88f341f159a
klintg <theklint1@gmail.com>
2016-04-26 23:43:29 +0300

    enviroment setup

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+import SearchBar from '../containers/search-bar.js'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+}
