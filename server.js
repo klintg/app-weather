85264e587f372722a8e6f4ec7319fc02971581e6
klintg <theklint1@gmail.com>
2016-07-01 18:05:48 +0300

    deploying the app

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path')
+var app = express()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+var port = process.env.PORT || 3000
+
+app.listen(port)
+
+console.log("app running");
