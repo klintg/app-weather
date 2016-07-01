54d77c93f8047976b45e70f4a204535868603fcc
klintg <theklint1@gmail.com>
2016-07-01 23:03:34 +0300

    adding gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var browserify = require("browserify")
+var reactify = require('reactify')
+var source = require('vinyl-source-stream')
+
+gulp.task ('browserify', function() {
+  browserify('./src/index.js')
+      .transform('babelify', {presets: 'react'})
+      .bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('dist'))
+})
+
+// gulp.task('copy', function() {
+//   gulp.src('src/js/vend/*.*')
+//     .pipe(gulp.dest('dist/js'))
+// })
+
+gulp.task('default', ['browserify'], function() {
+  return gulp.watch('src/**/*.*', ['browserify','copy'])
+})
